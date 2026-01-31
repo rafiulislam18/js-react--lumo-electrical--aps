a345c6ceab42d9adb50081c48b969991b4454678
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2026-01-31 07:38:26 +0600

    fix: fix orders status

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -47,14 +47,12 @@ export default function Orders() {
 
   const getStatusColor = (status: string) => {
     switch (status) {
-      case "Delivered":
-        return "bg-green-100 text-green-800";
-      case "Processing":
+      case "order_placed":
         return "bg-blue-100 text-blue-800";
-      case "Shipped":
+      case "out_for_delivery":
         return "bg-orange-100 text-orange-800";
-      case "Pending Payment":
-        return "bg-red-100 text-red-800";
+      case "delivered":
+        return "bg-green-100 text-green-800";
       default:
         return "bg-gray-100 text-gray-800";
     }
